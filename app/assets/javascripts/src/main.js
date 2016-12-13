1ba0a9e13227502409af1d1a9df111d475d10aa8
Guillaume Suard <geex@myself.com>
2016-12-13 19:06:30 +0100

    [TASK] Capistrano deployment test

--- app/assets/javascripts/src/main.js
+++ app/assets/javascripts/src/main.js
@@ -14,6 +14,10 @@ $(function() {
 		}
 	});
 
+	if (location.href = 'http://vps344707.ovh.net/en/donate') {
+		console.log('Capistrano test has passed successfully !');
+	}
+
 	/**
 	 * Mobile menu
 	 */
